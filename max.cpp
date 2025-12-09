f7a33e63d08258827baddbb87a2406c8c1471194
swarnakamalavarasala-993 <swarnakamalavarasala@gmail.com>
2025-12-09 11:07:51 +0530

    Add files via upload

---
+++ max.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, max, min;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    max = min = arr[0];   // assume first element as max & min
+
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+
+        if(arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Maximum = %d\n", max);
+    printf("Minimum = %d\n", min);
+
+    return 0;
+}
+
